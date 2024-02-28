37ab962356d51ae0c667192fe94c047fed48408f
Mawuawoe <desmonddzakago@gmail.com>
2024-02-28 21:35:02 +0000

    task 0

--- my_shell.c
+++ my_shell.c
@@ -8,10 +8,17 @@
 
 int main(int ac, char **argv)
 {
-(void)ac;
+    (void) ac;
+if (isatty(STDIN_FILENO) == 1)
+{
 while (1)
 {
 display_prompt();
+read_input(&argv[0]);
+}
+}
+else
+{
 read_input(&argv[0]);
 }
 return (0);
