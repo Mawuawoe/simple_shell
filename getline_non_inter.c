fa149b944e70772d3e1f8c5b98029358c6ffd803
Mawuawoe <desmonddzakago@gmail.com>
2024-03-02 13:08:19 +0000

    task 1

--- getline_non_inter.c
+++ getline_non_inter.c
@@ -10,7 +10,7 @@ int read_input_non_inter(char **av)
 char buff[100]; /*buff to store the user input*/
 char *read;
 char *trim_buff;
-(void) av;
+/*(void) av;*/
 read = fgets(buff, sizeof(buff), stdin);
 while (read != NULL)
 {
@@ -24,9 +24,9 @@ if (buff[(_strlen(buff) - 1)] == '\n')
 buff[(_strlen(buff) - 1)] = '\0';
 }
 trim_buff = remove_spaces(buff);
-printf("out here: %s\n",trim_buff);
+/*printf("out here: %s\n",trim_buff);*/
 /*tokenize(buff, av);*/
-/*excute_1(trim_buff, av);*/
+excute_1(trim_buff, av);
 read = fgets(buff, sizeof(buff), stdin);
 free(trim_buff);
 trim_buff = NULL;
